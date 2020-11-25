d79b5c0c22d0f15560b4994992d2fc25b4f1fb8f
rkumarp <issac7@gmail.com>
2020-11-25 03:00:43 -0600

    added ticks

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   window.addMe = window.addMe
     ? null
     : (x) => {
-        console.log(`Added - ${x}`);
+        // console.log(`Added - ${x}`);
         const li = document.createElement("button");
         li.id = x;
         li.innerText = x;
@@ -14,22 +14,24 @@
   window.printMe = window.printMe
     ? null
     : (x) => {
-        console.log(x);
-        document.getElementById("details").innerHTML = "";
+        // console.log(x);
+        // document.getElementById("details").innerHTML = "";
         l = document.createElement("div");
         l.id = "details-box";
         xd = [
-          `Name : ${x.a_ticker}`,
-          `Action : ${x.a}`,
+          `Name : ${x.a_ticker} [${x.a}]`,
+          `Median Growth : ${x.a_x.median}`,
           `Current Direction : ${x.a_curr_dirn}`,
-          `Freq:  ${x.a_freq}`,
-          `Prediction High: ${x.a_predict.high}`,
-          `Prediction Low: ${x.a_predict.low}`,
-          `Regression Value: ${x.a_predict.value}`,
+          `Current Gain : ${parseFloat((x.a_x.current - 1) * 100).toFixed(2)}%`,
+          `Current Price : $ ${x.close.current}`,
+          `Prediction High: $ ${x.a_predict.high}`,
+          `Prediction Low: $ ${x.a_predict.low}`,
+          `Regression Value: $ ${x.a_predict.value}`,
           `Strength: ${x.a_strength}`,
-          `Growth: ${x.a_x.ci.filter((e) => e.sigma === 2)[0].low} - ${
-            x.a_x.ci.filter((e) => e.sigma === 2)[0].high
-          }`,
+          `Growth (2 Sigma): ${
+            x.a_x.ci.filter((e) => e.sigma === 2)[0].low
+          } - ${x.a_x.ci.filter((e) => e.sigma === 2)[0].high}`,
+          `Freq:  ${x.a_freq}`,
         ];
         xd.forEach((lx) => {
           line = document.createElement("div");
@@ -37,14 +39,14 @@
           line.innerText = lx;
           l.appendChild(line);
         });
-        console.log(xd.join("\n"));
+        // console.log(xd.join("\n"));
         document.getElementById("details").appendChild(l);
       };
 
   window.fetchMe = window.fetchMe
     ? null
     : (x1) => {
-        console.log(`Fetching - ${x1}`);
+        // console.log(`Fetching - ${x1}`);
         axios
           .get(`http://localhost:5000/api/v1/data/${x1}`, { timeout: 5000 })
           .then((x) => x.data)
@@ -61,18 +63,11 @@
   ccc.appendChild(ul);
   ccc.appendChild(data);
 
-  ["AMD", "X", "INTL"].forEach((e) => {
-    // console.log(e);
-    addMe(e);
-  });
+  axios
+    .get("http://localhost:5000/api/v1/list")
+    .then((e) => e.data.data)
+    .then((x) => {
+      x.forEach(addMe);
+    })
+    .then(console.error);
 })();
-
-// sleep(1000);
-//     axios
-//       .get(`http://localhost:5000/api/v1/data/${e}`, { timeout: 5000 })
-//       .then((x) => x.data)
-//       .then((e) => {
-//         aa(e);
-//       })
-
-//       .catch(console.err);
